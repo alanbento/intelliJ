91ba2327b524ed7821d2316ad00055a03c6f125d
Alan.Bento <alangs.bento@gmail.com>
2021-11-04 17:52:27 -0300

    First Commid in Branch IntelliJ

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -5,8 +5,8 @@ import br.com.dio.model.Cat;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
 
-
-
+        Livro livro = new Livro("A Crônica do Matador de Reis, O nome do vento", 320);
+        System.out.println(livro);
     /*    int a = 5;
         int b = 3;
 
@@ -18,5 +18,40 @@ public class PrimeiroPrograma {
         System.out.println(gato);
 
 
+    }
+}
+
+
+class Livro {
+    private String nome;
+    private Integer numPag;
+
+    public Livro(String nome, Integer numPag) {
+        this.nome = nome;
+        this.numPag = numPag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPag() {
+        return numPag;
+    }
+
+    public void setNumPag(Integer numPag) {
+        this.numPag = numPag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPag=" + numPag +
+                '}';
     }
 }
